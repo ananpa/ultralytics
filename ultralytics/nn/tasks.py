3351923bbb13652b772da49cd6a871d2b18f67e8
Anan Panphuech <anan.panp@gmail.com>
2025-05-21 21:15:28 +0700

    Update tasks

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -77,6 +77,7 @@ from ultralytics.utils.torch_utils import (
     scale_img,
     time_sync,
 )
+from ultralytics.nn.modules.activation import GSigmoidV1
 from ultralytics.nn.modules.activation import Smish
 from ultralytics.nn.modules.activation import ReLUELU
 from ultralytics.nn.modules.activation import ReLUSiLU
